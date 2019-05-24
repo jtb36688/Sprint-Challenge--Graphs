44dc0d653611c1310651fc685c75e97a8a48c89b
Jacob Bryan <jtb36688@gmail.com>
2019-05-24 16:53:46 -0400

    allowing tests to pass with under 1000 moves after setting directions to a certain preferred order

--- graph_adventure/adv.py
+++ graph_adventure/adv.py
@@ -29,7 +29,7 @@ traversalPath = []
 # TRAVERSAL TEST
 visited_rooms = set()
 player.currentRoom = world.startingRoom
-directions = ['n', 's', 'e', 'w']
+directions = ['n', 's', 'w', 'e']
 while len(visited_rooms) != len(roomGraph):
     if player.currentRoom not in visited_rooms:
         visited_rooms.add(player.currentRoom)
@@ -80,34 +80,26 @@ while len(visited_rooms) != len(roomGraph):
             if room_in_dir and room_in_dir not in visited_rooms:
                 # print(searchpath[current_room.id])
                 for movement in searchpath[current_room.id]:
-                    attr2 = movement + '_to'
-                    searchdir = getattr(searchstart, attr2)
-                    searchstart = searchdir
-                    print("adding to visited rooms", searchstart.id)
-                    if searchstart not in visited_rooms:
-                        visited_rooms.add(searchstart)
+                    print("appending", movement)
+
                     traversalPath.append(movement)
-                    print("travpathlen", len(traversalPath))
-                    print("numrooms", len(visited_rooms))
+
+                print("appending", direction)
                 traversalPath.append(direction)
                 player.currentRoom = room_in_dir
                 room_found = True
                 break
         if room_found:
-            # print("closest unexplored", player.currentRoom.id)
+            print("closest unexplored", len(traversalPath))
             break
 # Moving in direction of found unexplored door and then adding path to searchPath
         for direction in directions:
             attr = direction + '_to'
             room_in_dir = getattr(current_room, attr)
             if room_in_dir and room_in_dir not in visited_search:
-                # print("line 93")
                 q.enqueue(room_in_dir)
-                if room_in_dir.id not in searchpath:
-                    # print("searchpath made with key", room_in_dir.id)
-                    searchpath[room_in_dir.id] = []
+                searchpath[room_in_dir.id] = []
                 if current_room.id in searchpath:
-                    # print("line 97")
                     for movement in searchpath[current_room.id]:
                         searchpath[room_in_dir.id].append(movement)
                 searchpath[room_in_dir.id].append(direction)
